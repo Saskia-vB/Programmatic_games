25d4de0a226ce8a42c711b60f30cfe0ce6ee065e
Saskia van Barthold <saskia.vanb@gmail.com>
2020-06-04 10:43:29 +0200

    completed fibunacci

--- fibunacci_calculator.py
+++ fibunacci_calculator.py
@@ -1,15 +1,13 @@
 # The Fibonacci numbers are the numbers in the following integer sequence.
 # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
 
+array = [5, 5, 6, 7, 8, 8]
 
-number_1 = 0
-number_2 = 1
+def no_duplicates(array):
+    new_array= []
+    for item in array:
+       if new_array.count(item) < 1:
+           new_array.append(item)
+    return new_array
 
-number_1 + number_2 = answer
-answer + number_2 = answer2
-answer2 + number_2 = answer3
-answer3 + number_2 = answer4
-answer4 +
-
-
-
+print(no_duplicates(array))
--- test_file.py
+++ test_file.py
@@ -1,4 +1,10 @@
-def name(argument):
-    print("Hi my name is " + argument)
+array = [5,5,4, 6, 7, 8, 9, 10, 10]
 
-name("Fahad")
+def no_duplicates(array):
+    new_array= []
+    for item in array:
+        if new_array.count(item)<1:
+            new_array.append(item)
+    return new_array
+
+print(no_duplicates(array))
